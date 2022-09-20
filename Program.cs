71e4a76511a74aba1356539438106f3abe25e643
Med <mohammed.ahmadi1990@gmail.com>
2022-09-20 12:52:13 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UniversityDataScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
